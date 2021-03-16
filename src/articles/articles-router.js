1bff21e94d7af989117c8dd2dbdf5160e0e87786
Kevin Johnson <kevinj902302@gmail.com>
2021-03-16 12:29:38 -0600

    Updates filepaths to ensure tests are passing

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const xss = require('xss')
 const ArticlesService = require('./articles-service')
+const path = require('path')
 
 const articlesRouter = express.Router()
 const jsonParser = express.json()
@@ -40,7 +41,7 @@ articlesRouter
       .then(article => {
         res
           .status(201)
-          .location(`/articles/${article.id}`)
+          .location(path.posix.join(req.originalUrl, `/${article.id}`))
           .json(serializeArticle(article))
       })
       .catch(next)
@@ -65,23 +66,37 @@ articlesRouter
       .catch(next)
   })
   .get((req, res, next) => {
-    res.json({
-      id: res.article.id,
-      style: res.article.style,
-      title: xss(res.article.title), // sanitize title
-      content: xss(res.article.content), // sanitize content
-      date_published: res.article.date_published,
-    })
+    res.json(serializeArticle(res.article))
   })
   .delete((req, res, next) => {
     ArticlesService.deleteArticle(
-       req.app.get('db'),
-       req.params.article_id
-     )
-       .then(() => {
-         res.status(204).end()
-       })
-       .catch(next)
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const articleToUpdate = { title, content, style }
+    const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'title', 'style' or 'content'`
+        }
+      })
+    ArticlesService.updateArticle(
+      req.app.get('db'),
+      req.params.article_id,
+      articleToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
   })
 
 module.exports = articlesRouter
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -19,11 +19,11 @@ describe('Articles Endpoints', function() {
 
   afterEach('cleanup',() => db('blogful_articles').truncate())
 
-  describe(`GET /articles`, () => {
+  describe(`GET /api/articles`, () => {
     context(`Given no articles`, () => {
       it(`responds with 200 and an empty list`, () => {
         return supertest(app)
-          .get('/articles')
+          .get('/api/articles')
           .expect(200, [])
       })
     })
@@ -39,7 +39,7 @@ describe('Articles Endpoints', function() {
 
       it('responds with 200 and all of the articles', () => {
         return supertest(app)
-          .get('/articles')
+          .get('/api/articles')
           .expect(200, testArticles)
       })
     })
@@ -50,7 +50,7 @@ describe('Articles Endpoints', function() {
       it(`responds with 404`, () => {
         const articleId = 123456
         return supertest(app)
-          .get(`/articles/${articleId}`)
+          .get(`/api/articles/${articleId}`)
           .expect(404, { error: { message: `Article doesn't exist` } })
       })
     })
@@ -68,7 +68,7 @@ describe('Articles Endpoints', function() {
         const articleId = 2
         const expectedArticle = testArticles[articleId - 1]
         return supertest(app)
-          .get(`/articles/${articleId}`)
+          .get(`/api/articles/${articleId}`)
           .expect(200, expectedArticle)
       })
     })
@@ -87,7 +87,7 @@ describe('Articles Endpoints', function() {
       })
         it('removes XSS attack content', () => {
         return supertest(app)
-          .get(`/articles/${maliciousArticle.id}`)
+          .get(`/api/articles/${maliciousArticle.id}`)
           .expect(200)
           .expect(res => {
             expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
@@ -106,7 +106,7 @@ describe('Articles Endpoints', function() {
         content: 'Test new article content...'
       }
       return supertest(app)
-        .post('/articles')
+        .post('/api/articles')
         .send(newArticle)
         .expect(201)
         .expect(res => {
@@ -114,14 +114,14 @@ describe('Articles Endpoints', function() {
           expect(res.body.style).to.eql(newArticle.style)
           expect(res.body.content).to.eql(newArticle.content)
           expect(res.body).to.have.property('id')
-          expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+          expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
           const expected = new Date().toLocaleString()
           const actual = new Date(res.body.date_published).toLocaleString()
           expect(actual).to.eql(expected)
         })
         .then(res =>
           supertest(app)
-            .get(`/articles/${res.body.id}`)
+            .get(`/api/articles/${res.body.id}`)
             .expect(res.body)
         )
     })
@@ -139,7 +139,7 @@ describe('Articles Endpoints', function() {
         delete newArticle[field]
 
         return supertest(app)
-          .post('/articles')
+          .post('/api/articles')
           .send(newArticle)
           .expect(400, {
             error: { message: `Missing '${field}' in request body` }
@@ -148,7 +148,7 @@ describe('Articles Endpoints', function() {
     })
   })
 
-  describe(`DELETE /articles/:article_id`, () => {
+  describe(`DELETE /api/articles/:article_id`, () => {
    context('Given there are articles in the database', () => {
      const testArticles = makeArticlesArray()
      beforeEach('insert articles', () => {
@@ -160,11 +160,11 @@ describe('Articles Endpoints', function() {
        const idToRemove = 2
        const expectedArticles = testArticles.filter(article => article.id !== idToRemove)
        return supertest(app)
-         .delete(`/articles/${idToRemove}`)
+         .delete(`/api/articles/${idToRemove}`)
          .expect(204)
          .then(res =>
            supertest(app)
-             .get(`/articles`)
+             .get(`/api/articles`)
              .expect(expectedArticles)
          )
      })
@@ -173,7 +173,7 @@ describe('Articles Endpoints', function() {
     it(`responds with 404`, () => {
       const articleId = 123456
       return supertest(app)
-        .delete(`/articles/${articleId}`)
+        .delete(`/api/articles/${articleId}`)
         .expect(404, { error: { message: `Article doesn't exist` } })
     })
   })
